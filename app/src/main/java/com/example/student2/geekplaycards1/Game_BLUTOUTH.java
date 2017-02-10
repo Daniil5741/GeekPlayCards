a569ad192713bde17198d7a3b59cad8e8da83f56
student2 <Daniil5741>
2017-02-10 16:33:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/student2/geekplaycards1/Game_BLUTOUTH.java
@@ -0,0 +1,7 @@
+package com.example.student2.geekplaycards1;
+
+/**
+ * Created by student2 on 20.01.17.
+ */
+public class Game_BLUTOUTH {
+}
---
+++ app/src/main/java/com/example/student2/geekplaycards1/MyService.java
@@ -0,0 +1,7 @@
+package com.example.student2.geekplaycards1;
+
+/**
+ * Created by student2 on 31.01.17.
+ */
+public class MyService {
+}
